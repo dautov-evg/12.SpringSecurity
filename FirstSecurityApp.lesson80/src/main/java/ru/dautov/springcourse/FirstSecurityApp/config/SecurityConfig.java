bab7d2405774358dcbb9cbd45a988f5da8dc1ba4
Dautov Evgeniy <evg.dautov@mail.ru>
2023-01-28 09:47:13 +0300

    Урок 80 - Аутентификация ч.2 (удаление AuthProvider)

--- FirstSecurityApp.lesson80/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp.lesson80/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,33 @@
 package ru.dautov.springcourse.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import ru.dautov.springcourse.FirstSecurityApp.security.AuthProviderImpl;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.dautov.springcourse.FirstSecurityApp.services.PersonDetailsService;
+
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {   // здесь вся конфигурация Spring Security
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
 
     //    Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService);
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
