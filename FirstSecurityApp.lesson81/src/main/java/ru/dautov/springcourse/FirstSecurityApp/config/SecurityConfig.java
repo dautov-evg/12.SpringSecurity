cff13df40515a18f528ae005ae9d97696a1b572c
Dautov Evgeniy <evg.dautov@mail.ru>
2023-01-29 08:14:20 +0300

    Урок 81 - Кастомная страница авторизации

--- FirstSecurityApp.lesson81/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp.lesson81/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package ru.dautov.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -20,8 +21,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {   // здес
         this.personDetailsService = personDetailsService;
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // конфигурируем сам Spring Security (вход, ошибки и т д)
 
-    //    Настраивает аутентификацию
+        http.csrf().disable() //отключили токе csrf(временно). Чтобы работало
+                // конфигурируем авторизацию
+                .authorizeRequests()
+                .antMatchers("/auth/login", "/error").permitAll()  //настраиваем чтобы на эти страницы могли войти даже неаутентифицированные пользователи
+                .anyRequest().authenticated()  //все остальные страницы доступны только через авторизацию
+                .and()
+//                конфигурируем страничку логина
+                .formLogin().loginPage("/auth/login")
+                .loginProcessingUrl("/process_login") //куда отправлять данные с формы (Spring проверет сам)
+                .defaultSuccessUrl("/hello", true) //куда направлять, в случае валидных данных
+                .failureUrl("/auth/login?error");    //Куда направлять в случае невалидных данных
+    }
+
+
+    //    Настраиваем аутентификацию
+    @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(personDetailsService);
     }
