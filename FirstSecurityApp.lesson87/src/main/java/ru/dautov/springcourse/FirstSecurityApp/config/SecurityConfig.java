4d79eb25cd2318d531f9c3a77fe89d9e2dfec99c
Dautov Evgeniy <evg.dautov@mail.ru>
2023-02-02 22:11:59 +0300

    Урок 87 - Аннотация @PreAuthorize

--- FirstSecurityApp.lesson87/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
+++ FirstSecurityApp.lesson87/src/main/java/ru/dautov/springcourse/FirstSecurityApp/config/SecurityConfig.java
@@ -3,16 +3,17 @@ package ru.dautov.springcourse.FirstSecurityApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ru.dautov.springcourse.FirstSecurityApp.services.PersonDetailsService;
 
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {   // здесь вся конфигурация Spring Security
 
     private final PersonDetailsService personDetailsService;
@@ -27,7 +28,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {   // здес
         // конфигурируем сам Spring Security (вход, ошибки и т д)
         // конфигурируем авторизацию
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
+//                .antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll() //настраиваем чтобы на эти страницы могли войти даже неаутентифицированные пользователи
                 .anyRequest().hasAnyRole("USER", "ADMIN")
 
--- FirstSecurityApp.lesson87/src/main/java/ru/dautov/springcourse/FirstSecurityApp/controllers/HelloController.java
+++ FirstSecurityApp.lesson87/src/main/java/ru/dautov/springcourse/FirstSecurityApp/controllers/HelloController.java
@@ -1,15 +1,25 @@
 package ru.dautov.springcourse.FirstSecurityApp.controllers;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import ru.dautov.springcourse.FirstSecurityApp.security.PersonDetails;
+import ru.dautov.springcourse.FirstSecurityApp.services.AdminService;
 
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String sayHello() {
         return "hello";
@@ -27,6 +37,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
